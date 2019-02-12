e037c48c828a4cb00dadeb83ddcd5cda676c75a7
Nolan <nolan.hellyer@gmail.com>
2019-02-11 20:27:42 -0800

    added LobbyGame and JoinButton components

---
+++ frontend/src/components/JoinButton.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { Button } from "reactstrap";
+
+const JoinButton = props =>
+  <Button>Join as { props.color }</Button>;
+
+export default JoinButton;
--- frontend/src/components/LobbyGame.js
+++ frontend/src/components/LobbyGame.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import JoinButton from "./JoinButton";
+
 const LobbyGame = props =>
   <tr>
     <td>{ props.game.id }</td>
-    <td> { props.game.playerWhite }</td>
-    <td> { props.game.playerBlack } </td>
+    <td> { props.game.playerWhite ? props.game.playerWhite : <JoinButton color="white" /> }</td>
+    <td> { props.game.playerBlack ? props.game.playerBlack : <JoinButton color="black"/> } </td>
   </tr>
 
 export default LobbyGame;
