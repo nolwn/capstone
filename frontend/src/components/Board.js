ff53ab0f619554c957ac3b57f7c287c4ea782bca
Nolan <nolan.hellyer@gmail.com>
2019-02-26 14:26:36 -0800

    fixed border issue

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -7,19 +7,10 @@ const highlights = [];
 const Board = props =>
   <div>
     <div style={{
-        border: 3 + "px white solid",
-        width: 64 * 8 + 3,
-        height: 64 * 8 + 3,
-        position: "absolute",
-        left: 0,
-        right: 0,
-        margin: "auto"
-      }}>
-    </div>
-    <div style={{
         width: 64 * 8 + "px",
         display: "flex",
-        margin: "auto"
+        margin: "auto",
+        outline: "3px white solid"
       }}>
       {
         [ ..."12345678" ]
